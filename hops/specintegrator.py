93b4c623886904f375ea1a6ab3720db72ab8be3b
Daniel Suess <daniel.suess@web.de>
2014-06-16 12:02:23 +0200

    Add option to continue old calculation

--- hops/specintegrator.py
+++ hops/specintegrator.py
@@ -27,8 +27,7 @@ from __future__ import division, print_function
 
 import numpy as np
 from numpy import complex128 as complex_t, float64 as float_t
-from scipy.sparse import csr_matrix as CSRMatrix
-from itertools import izip
+from scipy.sparse import csr_matrix as CSRMatrix, lil_matrix as LILMatrix
 from scipy.integrate import ode
 
 from hops.hstruct import HierarchyStructure, INVALID_INDEX
@@ -58,7 +57,6 @@ class SpectrumHierarchyIntegrator(object):
         Keyword arguments:
         with_terminator -- Use terminator for truncated aux. states
         """
-        self._struct = struct
         self._g = np.ravel(bath.get('g')).astype(complex_t)
         self._gamma = np.ravel(bath.get('gamma')).astype(float_t)
         self._omega = np.ravel(bath.get('Omega')).astype(float_t)
@@ -67,25 +65,31 @@ class SpectrumHierarchyIntegrator(object):
         self._h_sys = np.asarray(h_sys, dtype=complex_t)
         self._with_terminator = with_terminator
 
-        self._prop = self._setup_linear_propagator()
-        self._prop.sum_duplicates()
-        self._prop.eliminate_zeros()
+        self._prop = self._setup_linear_propagator(struct)
 
-    def _setup_linear_propagator(self):
+    @property
+    def nr_equations(self):
+        return self._prop.shape[0]
+
+    def _setup_linear_propagator(self, struct):
         """Sets up the noise-independent, linear propagator
 
         Returns:
         The propagator as CSR Matrix
         """
-        I, J, A = hcons.setup_linear_propagator(self._struct.vecind,
-                                                self._struct.indab,
-                                                self._struct.indbl,
+        i, j, a = hcons.setup_linear_propagator(struct.vecind,
+                                                struct.indab,
+                                                struct.indbl,
                                                 self._h_sys,
                                                 self._g,
                                                 self._gamma + 1.j*self._omega,
                                                 self._l_map,
                                                 self._with_terminator)
-        return CSRMatrix((A, J, I))
+        csr = CSRMatrix((a, j, i))
+        csr.sum_duplicates()
+        csr.eliminate_zeros()
+        return csr
+
         # dim = self._h_sys.shape[0]
         # nr_aux = self._struct.entries
         # size = dim * nr_aux
@@ -98,7 +102,7 @@ class SpectrumHierarchyIntegrator(object):
         #     prop[ent * dim:(ent+1) * dim, ent * dim:(ent+1) * dim] \
         #             = -1.j * self._h_sys - np.dot(k, w) * np.identity(dim)
 
-        #     mode_iter = enumerate(izip(self._l_map, self._g, self._gamma,
+        #     mode_iter = enumerate(zip(self._l_map, self._g, self._gamma,
         #                                self._omega))
         #     for mode, (l, g, gamma, omega) in mode_iter:
         #         ind = self._struct.indbl[ent, mode]
@@ -108,55 +112,57 @@ class SpectrumHierarchyIntegrator(object):
         #         ind = self._struct.indab[ent, mode]
         #         if ind != INVALID_INDEX:
         #             prop[dim * ent + l, dim * ind + l] += -1.
-        #         elif self._with_terminator:
-        #             for ent_t, mode_t, val_t in self._terminator(ent, mode):
-        #                 prop[dim * ent + self._l_map[mode_t],
-        #                      dim * ent_t + self._l_map[mode_t]] += val_t
-        # return prop.tocsr()
+        #         # elif self._with_terminator:
+        #         #     for ent_t, mode_t, val_t in self._terminator(ent, mode):
+        #         #         prop[dim * ent + self._l_map[mode_t],
+        #         #              dim * ent_t + self._l_map[mode_t]] += val_t
+        # prop.tocsr()
 
-    def _terminator(self, iind, mode):
-        """Returns the terminator replacement for psi[k + e_mode], where
-        k = vecind[iind]:
-                psi[k+e_j] = sum_i (k+e_j)_i g_i / dot(k+e_j, w)        (*)
-                                       * l_map[i] psi[k+e_j-e_i]
+        # # return prop.tocsr()
 
-        iind -- Integer index for the current aux state (k)
-        mode -- Mode for which k is increased by one
+    # def _terminator(self, iind, mode):
+    #     """Returns the terminator replacement for psi[k + e_mode], where
+    #     k = vecind[iind]:
+    #             psi[k+e_j] = sum_i (k+e_j)_i g_i / dot(k+e_j, w)        (*)
+    #                                    * l_map[i] psi[k+e_j-e_i]
 
-        Returns:
-        List of tuples (iind_t, mode_t, val_t), where
-        iind_t -- Integer index for (k + e_j - e_i) (j=mode, i=mode_t)
-        mode_t -- Mode which is subtracted from (k + e_j)
-        val_t  -- Prefactor for terminator (i.e. everything in the first line
-                  of (*))
-        """
-        # TODO Remake with dict lookup
-        nr_modes = len(self._g)
-        res = []
-        w = self._gamma + 1.j * self._omega
-        e = np.zeros(nr_modes, dtype=complex_t)
-        e[mode] = 1
-        prefac = lambda k, i: -(k[i] + e[i]) * self._g[i] / np.dot(k + e, w)
+    #     iind -- Integer index for the current aux state (k)
+    #     mode -- Mode for which k is increased by one
 
-        for mode_t in xrange(nr_modes):
-            if mode_t == mode:
-                res.append((iind, mode, prefac(self._struct.vecind[iind],
-                                               mode)))
-                continue
+    #     Returns:
+    #     List of tuples (iind_t, mode_t, val_t), where
+    #     iind_t -- Integer index for (k + e_j - e_i) (j=mode, i=mode_t)
+    #     mode_t -- Mode which is subtracted from (k + e_j)
+    #     val_t  -- Prefactor for terminator (i.e. everything in the first line
+    #               of (*))
+    #     """
+    #     # TODO Remake with dict lookup
+    #     nr_modes = len(self._g)
+    #     res = []
+    #     w = self._gamma + 1.j * self._omega
+    #     e = np.zeros(nr_modes, dtype=complex_t)
+    #     e[mode] = 1
+    #     prefac = lambda k, i: -(k[i] + e[i]) * self._g[i] / np.dot(k + e, w)
 
-            if self._struct.indbl[iind, mode_t] == INVALID_INDEX:
-                continue
+    #     for mode_t in xrange(nr_modes):
+    #         if mode_t == mode:
+    #             res.append((iind, mode, prefac(self._struct.vecind[iind],
+    #                                            mode)))
+    #             continue
 
-            iind_t = self._struct.indab[self._struct.indbl[iind, mode_t], mode]
-            if iind_t == INVALID_INDEX:
-                continue
+    #         if self._struct.indbl[iind, mode_t] == INVALID_INDEX:
+    #             continue
 
-            res.append((iind_t, mode_t, prefac(self._struct.vecind[iind],
-                                               mode_t)))
+    #         iind_t = self._struct.indab[self._struct.indbl[iind, mode_t], mode]
+    #         if iind_t == INVALID_INDEX:
+    #             continue
 
-        return res
+    #         res.append((iind_t, mode_t, prefac(self._struct.vecind[iind],
+    #                                            mode_t)))
 
-    def get_trajectory(self, t_length, t_steps, psi0=None, extern=True):
+    #     return res
+
+    def get_trajectory(self, t_length, t_steps, psi0=None):
         """Calculates the solution of the linear hierarchy without noise
 
         t_length -- full propagation time
@@ -164,40 +170,32 @@ class SpectrumHierarchyIntegrator(object):
 
         Keyword arguments:
         psi0[dim] -- initial state of the system (default [1,1,...]/sqrt(dim))
+                     (alternatively pass array of size self.nr_equations and
+                      dype complex_t, which is used as computing state. After
+                      propagation it contains the full hierarchy state, which
+                      can be used to continue propagation.)
         extern    -- use compiled integrator (default True)
 
         Returns:
         psi[t_steps, dim] -- Trajectory for Z_t = 0
         """
         dim = self._h_sys.shape[0]
-        psi_init = np.zeros(dim * self._struct.entries, dtype=complex_t)
-        psi_init[0:dim] = psi0 if psi0 is not None \
-                else np.ones(dim, dtype=complex_t) / np.sqrt(dim)
-        if extern:
-            psi = hint.calc_trajectory_lin(t_length, t_steps, dim, psi_init,
-                                           self._prop.indptr,
-                                           self._prop.indices,
-                                           self._prop.data)
-            return np.transpose(psi)
-        else:
-            dt = t_length / (t_steps - 1)
+        if psi0 is None:
+            psi0 = np.ones(dim, dtype=complex_t) / np.sqrt(dim)
+        if psi0.size == dim:
+            psi0 = np.append(psi0, np.zeros(self.nr_equations - dim,
+                                            dtype=complex_t))
+        elif psi0.size != self.nr_equations:
+            raise RuntimeError("specintegrator.py:get_trajectory: psi0 has wrong shape")
 
-            rhs = lambda t, y: self._prop.dot(y)
-            psi = np.empty([t_steps, dim], dtype=complex_t)
-            psi[0] = psi_init[:dim]
-            integ = ode(rhs).set_integrator('zvode', method='bdf')
-            integ.set_initial_value(psi_init, 0.0)
-            i = 1
+        psi = hint.calc_trajectory_lin(t_length, t_steps, dim, psi0,
+                                       self._prop.indptr,
+                                       self._prop.indices,
+                                       self._prop.data)
+        return np.transpose(psi)
 
-            while integ.successful() and i < t_steps:
-                integ.integrate(integ.t + dt)
-                psi[i] = integ.y[:dim]
-                i += 1
-
-            return psi
-
-    def get_spectrum(self, dw, t_steps, psi0=None, extern=True, wmin=None,
-                     wmax=None, sigma_w=0):
+    def get_spectrum(self, dw, t_steps, psi0=None, wmin=None, wmax=None,
+                     sigma_w=0):
         """Calculates the spectrum using the linear hierarchy without noise
 
         dw      -- Resolution of the spectrum in frequency space
@@ -218,7 +216,7 @@ class SpectrumHierarchyIntegrator(object):
         A[:] -- Spectrum A(w)
         """
         t_length = 2 * np.pi / dw
-        psi = self.get_trajectory(t_length, t_steps, psi0, extern)
+        psi = self.get_trajectory(t_length, t_steps, psi0)
         dt = t_length / (t_steps - 1)
         corr = np.sum(psi * np.conj(psi[0]), axis=1)
         w, A = specfun.fourier(corr, dt, output_w=True, hermitian=True,
@@ -250,8 +248,7 @@ def simple_spectrum_hierarchy(depth, g, gamma, omega, h_sys, **kwargs):
 
     """
     dim = np.shape(h_sys)[0]
-    with Timer('Structure'):
-        struct = HierarchyStructure(len(g) * dim, depth)
+    struct = HierarchyStructure(len(g) * dim, depth)
     bath = {'g': [g for _ in range(dim)],
             'gamma': [gamma for _ in range(dim)],
             'Omega': [omega for _ in range(dim)]}
@@ -260,10 +257,10 @@ def simple_spectrum_hierarchy(depth, g, gamma, omega, h_sys, **kwargs):
 
 if __name__ == '__main__':
     from timer import Timer
-    hier = simple_spectrum_hierarchy(5, [1, 1], [2, 2], [3, 3],
-                                     np.zeros([7, 7]))
+    hier = simple_spectrum_hierarchy(1, [1, 1], [2, 2], [3, 3],
+                                     np.zeros([2, 2]))
     print(hier._struct.entries)
-    # with Timer('Extern'):
-    #     hier.get_trajectory(100, 20000, extern=True)
     # with Timer('Intern'):
-    #     hier.get_trajectory(100, 20000)
+    #     hier.get_trajectory(100, 20000, extern=False)
+    with Timer('Extern'):
+        hier.get_trajectory(100, 20000, extern=True)
--- hops/todeint.f90
+++ hops/todeint.f90
@@ -8,12 +8,11 @@ implicit none
 
 ! RUNGE-KUTTA PARAMETERS
 integer, parameter :: &
-      RK_COPIES = 5, &     ! Number of copies needed
-      RK_C = 1, &
-      RK_1 = 2, &
-      RK_2 = 3, &
-      RK_3 = 4, &
-      RK_4 = 5
+      RK_COPIES = 4, &     ! Number of copies needed
+      RK_1 = 1, &
+      RK_2 = 2, &
+      RK_3 = 3, &
+      RK_4 = 4
 
 contains
 
@@ -22,8 +21,10 @@ contains
 ! dependent parameter f, which is passed to the rhs-method and processed by the
 ! latter.
 !
+! It calculates on the y0-memory passed in.
+!
 ! TODO output_crop can be array of ints
-function zintegrate_rk4(rhs, t_length, t_steps, f, y0, output_crop) result(res)
+function zintegrate_rk4(rhs, t_length, t_steps, f, y, output_crop) result(res)
    implicit none
    interface
       subroutine rhs(y, f, ydot)
@@ -34,42 +35,41 @@ function zintegrate_rk4(rhs, t_length, t_steps, f, y0, output_crop) result(res)
       end subroutine rhs
    end interface
 
-   real(dp), intent(in)    :: t_length
-   integer(ki), intent(in) :: t_steps
-   complex(dp), intent(in) :: f(:, :)
-   complex(dp), intent(in) :: y0(:)
-   integer(ki), intent(in) :: output_crop
-   complex(dp)             :: res(output_crop, t_steps)
+   real(dp), intent(in)       :: t_length
+   integer(ki), intent(in)    :: t_steps
+   complex(dp), intent(in)    :: f(:, :)
+   complex(dp), intent(inout) :: y(:)
+   integer(ki), intent(in)    :: output_crop
+   complex(dp)                :: res(output_crop, t_steps)
    !----------------------------------------------------------------------------
    real(dp) :: dt
-   complex(dp) :: y(size(y0), RK_COPIES)
+   complex(dp) :: yrk(size(y), RK_COPIES)
    integer(ki) :: t
 
    ! TODO ERROR CHECK shape(f) = [nr_procs, 2*tSteps]
 
    dt = t_length / (t_steps - 1)
-   y(:, RK_C) = y0
-   res(1:output_crop, 1) = y0(1:output_crop)
+   res(1:output_crop, 1) = y(1:output_crop)
 
    do t = 2, t_steps
-      call rhs(y(:, RK_C), f(:, 2*t - 2), y(:, RK_1))
-      y(:, RK_1) = dt * y(:, RK_1)
+      call rhs(y(:), f(:, 2*t - 2), yrk(:, RK_1))
+      yrk(:, RK_1) = dt * yrk(:, RK_1)
 
-      call rhs(y(:, RK_C) + .5 * y(:, RK_1), f(:, 2*t - 1), y(:, RK_2))
-      y(:, RK_2) = dt * y(:, RK_2)
+      call rhs(y(:) + .5 * yrk(:, RK_1), f(:, 2*t - 1), yrk(:, RK_2))
+      yrk(:, RK_2) = dt * yrk(:, RK_2)
 
-      call rhs(y(:, RK_C) + .5 * y(:, RK_2), f(:, 2*t - 1), y(:, RK_3))
-      y(:, RK_3) = dt * y(:, RK_3)
+      call rhs(y(:) + .5 * yrk(:, RK_2), f(:, 2*t - 1), yrk(:, RK_3))
+      yrk(:, RK_3) = dt * yrk(:, RK_3)
 
-      call rhs(y(:, RK_C) + y(:, RK_3), f(:, 2*t), y(:, RK_4))
-      y(:, RK_4) = dt * y(:, RK_4)
+      call rhs(y(:) + yrk(:, RK_3), f(:, 2*t), yrk(:, RK_4))
+      yrk(:, RK_4) = dt * yrk(:, RK_4)
 
-      y(:, RK_C) = y(:, RK_C) &
-            + 1./6. * y(:, RK_1) &
-            + 2./6. * y(:, RK_2) &
-            + 2./6. * y(:, RK_3) &
-            + 1./6. * y(:, RK_4)
-      res(:, t) = y(1:output_crop, RK_C)
+      y(:) = y(:) &
+            + 1./6. * yrk(:, RK_1) &
+            + 2./6. * yrk(:, RK_2) &
+            + 2./6. * yrk(:, RK_3) &
+            + 1./6. * yrk(:, RK_4)
+      res(:, t) = y(1:output_crop)
    end do
 end function zintegrate_rk4
 
