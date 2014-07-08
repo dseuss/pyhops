Implement fermionic master equation (WRONG RESULTS)

finite_fermionic.py
@@ -21,6 +21,7 @@ import scipy.sparse as sp
 from itertools import izip
 
 import physics.qustat as qs
+import mhops.fermionic as fm
 from tools.sci import zodeint
 
 
@@ -97,8 +98,8 @@ def analytic_rho12(t, psi0, omega, strength):
 if __name__ == '__main__':
     sigma_z = np.diag((1, -1))
     sigma_m = np.array([[0, 0], [1, 0]])
-    prop = -1.j * full_hamiltonian(.5 * sigma_z, sigma_m, [1.], [10.])
-    t, dt = np.linspace(0, 10, 1000, retstep=True)
+    prop = -1.j * full_hamiltonian(.5 * sigma_z, sigma_m, [1.], [1.])
+    t, dt = np.linspace(0, 1, 1000, retstep=True)
     psi_0_sys = [.6, .8]
     psi0 = full_state(psi_0_sys, 1).astype(complex)
     t, psi = zodeint(lambda t, y: prop.dot(y), psi0, t,
@@ -107,6 +108,15 @@ if __name__ == '__main__':
     rho = np.asarray([reduced_rho(psi_t, 2) for psi_t in psi])
     print(rho.shape)
 
-    pl.plot(t, np.real(rho[:, 0, 0]))
-    pl.plot(t, np.real(rho[:, 1, 1]))
+    cl = ['r', 'b']
+    for i in range(2):
+        pl.plot(t, np.real(rho[:, i, i]), ls='--', color=cl[i])
+
+    trace = lambda rho: np.sum(np.diagonal(rho, axis1=1, axis2=2), axis=1)
+    meq = fm.FermionicIntegrator({'g': [[1.]], 'gamma': [[0.]], 'Omega': [[1.]]},
+                                 .5 * sigma_z, couplops=[sigma_m])
+    t, rho = meq.get_rho(dt, t[-1], psi0=np.array(psi_0_sys))
+    rho /= trace(rho)[:, None, None]
+    for i in range(2):
+        pl.plot(t, np.real(rho[:, i, i]), ls=':', color=cl[i])
     pl.show()

mhops/_base.py
@@ -48,7 +48,7 @@ class MasterIntegrator(object):
 
     """Docstring for MasterEqIntegrator. """
 
-    def __init__(self, bath, h_sys):
+    def __init__(self, bath, h_sys, couplops):
         """@todo: Docstring for __init__.
 
         bath -- TODO
@@ -56,16 +56,14 @@ class MasterIntegrator(object):
         Returns:
 
         """
-        self._g = np.ravel(bath.get('g')).astype(complex_t)
-        self._gamma = np.ravel(bath.get('gamma')).astype(float_t)
-        self._omega = np.ravel(bath.get('Omega')).astype(float_t)
+        self._g = np.ravel(bath['g']).astype(complex_t)
+        self._gamma = np.ravel(bath['gamma']).astype(float_t)
+        self._omega = np.ravel(bath['Omega']).astype(float_t)
         self._modes = self._g.size
         self._l_map = np.ravel([np.ones(len(g), dtype=int_t) * i
                                 for i, g in enumerate(bath['g'])])
         self._h_sys = np.ascontiguousarray(h_sys, dtype=complex_t)
-        self._l = np.zeros((self.dim_hs, ) * 3)
-        for i in range(self.dim_hs):
-            self._l[i, i, i] = 1.
+        self._l = np.asarray(couplops)
 
         self._nr_aux_states = None
         self._prop = None

mhops/fermionic.py
@@ -3,40 +3,50 @@
 
 from __future__ import division, print_function
 import numpy as np
-import scipy.sparse as sparse
+import scipy.sparse as sp
 
 from hops.hstruct import HierarchyStructure
 from ._base import MasterIntegrator, commutator, multiply_raveled, complex_t
 
 
-class BosonicIntegrator(MasterIntegrator):
+class FermionicIntegrator(MasterIntegrator):
 
-    """Docstring for BosonicIntegrator. """
+    """Docstring for FermionicIntegrator. """
 
-    def __init__(self, depth, bath, h_sys, couplops):
-        """@todo: to be defined1. """
+    def __init__(self, bath, h_sys, couplops, pop_modes=None):
+        """@todo: to be defined1.
+
+        :param bath: @todo
+        :param h_sys: @todo
+        :param pop_modes: @todo
+
+        """
         MasterIntegrator.__init__(self, bath, h_sys, couplops)
 
-        struct = HierarchyStructure(2 * self._modes, depth)
+        self._pop_modes = pop_modes if pop_modes is not None else self._modes
+        struct = HierarchyStructure(2 * self._modes, 1, mode='quadratic',
+                                    pop_modes=2 * self._pop_modes)
         self._nr_aux_states = struct.entries
         self._prop = self._setup_propagator(struct)
 
     def _setup_propagator(self, struct):
         """@todo: Docstring for _setup_propagator.
 
-        struct -- TODO
-        Returns:
+        :param struct: @todo
+        :returns: @todo
 
         """
         # TODO THIS ASSUMES THAT INVALID_INDEX < 0; INCORPORATE INVALID_INDEX
-        prop = sparse.lil_matrix((self.dim_aux, self.dim_aux), dtype=complex_t)
+        prop = sp.lil_matrix((self.dim_aux, self.dim_aux), dtype=complex_t)
         dim_rho = self.dim_hs**2
         w = self._gamma + 1.j * self._omega
+        adj = lambda A: np.conj(A.T)
 
         for iind in xrange(self.nr_aux_states):
             base = iind * dim_rho
             m = struct.vecind[iind, :self._modes]
             n = struct.vecind[iind, self._modes:]
+
             prop[base:base + dim_rho, base:base + dim_rho] += \
                 -1.j * commutator(self._h_sys) \
                 - (np.dot(m, w) + np.dot(n, np.conj(w))) * np.identity(dim_rho)
@@ -46,43 +56,40 @@ class BosonicIntegrator(MasterIntegrator):
 
                 base_c = struct.indbl[iind, mode] * dim_rho
                 if base_c >= 0:
+                    pref = (-1)**(sum(m[mode:])) * m[mode] * self._g[mode]
                     prop[base:base + dim_rho, base_c:base_c + dim_rho] += \
-                        multiply_raveled(m[mode] * self._g[mode] * couplop,
-                                         side='l')
+                        multiply_raveled(pref * couplop, side='l')
 
                 base_c = struct.indbl[iind, self._modes + mode] * dim_rho
                 if base_c >= 0:
+                    pref = (-1)**(sum(n[mode:])) * n[mode] * np.conj(self._g[mode])
                     prop[base:base + dim_rho, base_c:base_c + dim_rho] += \
-                        multiply_raveled(n[mode] * np.conj(self._g[mode]) *
-                                         np.conj(couplop.T), side='r')
+                        multiply_raveled(pref * adj(couplop), side='l')
 
                 base_c = struct.indab[iind, mode] * dim_rho
                 if base_c >= 0:
                     prop[base:base + dim_rho, base_c:base_c + dim_rho] += \
-                        - commutator(np.conj(couplop.T))
+                        multiply_raveled((-1)**(sum(m[mode:]) + 1) * adj(couplop), side='l') \
+                        + multiply_raveled((-1)**sum(n) * adj(couplop), side='r')
 
                 base_c = struct.indab[iind, self._modes + mode] * dim_rho
                 if base_c >= 0:
                     prop[base:base + dim_rho, base_c:base_c + dim_rho] += \
-                        commutator(couplop)
+                        multiply_raveled((-1)**(sum(m) + 1) * couplop, side='l') \
+                        - multiply_raveled((-1)**sum(n[mode:]) * couplop,
+                                           side='r')
 
         return prop.tocsr()
 
 
-def simple_bosonic_meq(depth, g, gamma, omega, h_sys):
-    """Spectrum hierarchy with identical, independent baths with bcf
-            alpha_i(t) = sum_j g_j * exp(-gamma_j|t| - ii * Omega_j * t)
+def simple_fermionic_meq(g, gamma, omega, h_sys):
+    """@todo: Docstring for simple_fermionic_meq.
 
-    depth          -- depth of the hierarchy
-    g[modes]       -- coupling strengths of the modes
-    gamma[modes]   -- damping sof the modes
-    omega[modes]   -- central frequencies of the modes
-    h_sys[dim,dim] -- system hamiltonian
-
-    Usage:
-    meq = master.simple_bosonic_meq(Depth, g, gamma, Omega, h)
-    dt = tLength / (tSteps - 1)
-    t, rho = meq.get_rho(dt, tLength)
+    :param g: @todo
+    :param gamma: @todo
+    :param omega: @todo
+    :param h_sys: @todo
+    :returns: @todo
 
     """
     dim = np.shape(h_sys)[0]
@@ -94,4 +101,4 @@ def simple_bosonic_meq(depth, g, gamma, omega, h_sys):
     for i in range(dim):
         couplops[i, i, i] = 1.
 
-    return BosonicIntegrator(depth, bath, h_sys, couplops)
+    return FermionicIntegrator(bath, h_sys, couplops)